cda4b78624d341647414878f916b3e5c1ca9a20f
Saul <shaul.s@campus.technion.ac.il>
2020-05-04 13:19:43 +0300

    coded initializeData function into PointCloud class

--- HW2/PointCloud.py
+++ HW2/PointCloud.py
@@ -1,5 +1,50 @@
 import vtk
-from numpy import hstack, array
+from tkinter.filedialog import askopenfilenames
+from numpy import hstack, array, concatenate, vstack
+
+
+class point3D:
+
+    def __init__(self, x, y, z):
+        """
+        Initialize a 3D point with x, y and z ordinates
+        :type x: float
+        :type y: float
+        :type z: float
+        """
+        # defining parameters
+        self.x = x  # x ordinate
+        self.y = y  # y ordinate
+        self.z = z  # z ordinate
+
+
+class PointCloud:
+
+    def __init__(self):
+        self.pts = None
+
+    def initializeData(self):
+        try:
+            point3Dfiles = askopenfilenames(title='Select Input File')
+        except:
+            print('Oops! something went wrong :(')
+
+        temp_points = []
+
+        for filename in point3Dfiles:
+            with open(filename) as file:
+                lines = file.readlines()
+                for line in lines:
+                    line = line.split()
+                    if len(line) < 3:
+                        continue
+                    else:
+                        temp_points.append(array(line[0:3]).astype(float))
+
+        self.pts = vstack(temp_points)
+
 
 if __name__ == '__main__':
-    pass
+    cloud = PointCloud()
+    cloud.initializeData()
+    print(cloud.pts.shape)
