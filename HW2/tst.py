e0e36eee7808f703e83a037b06519eaa74a8ef6c
Saul <shaul.s@campus.technion.ac.il>
2020-05-08 10:56:19 +0300

    fixed kdtree initialization needs to be in two dimensions

--- HW2/tst.py
+++ HW2/tst.py
@@ -6,7 +6,6 @@ from HW2.GeoEqualCells import *
 import time
 
 
-
 def naiveSearch(radius, threshold, cloud):
     """
     performs a naive linear search across cloud of points to apply the morphological filter
@@ -22,11 +21,11 @@ def naiveSearch(radius, threshold, cloud):
     terrain = []
     objects = []
 
-    for i, p in enumerate(cloud):
+    for p in cloud:
         # distance from p to every point in the cloud
         dist = sqDist(p, cloud)
         # finding the points inside the radius
-        pointsInRadius = cloud[dist<radius**2,:]
+        pointsInRadius = cloud[dist < radius * radius, :]
         # checking if a point is object or terrain
         if isObject(p, pointsInRadius, threshold):
             objects.append(p)
@@ -35,13 +34,13 @@ def naiveSearch(radius, threshold, cloud):
 
     return terrain, objects
 
+
 def KDTreeSearch(radius, threshold, cloud, kdtree):
     start = time.time()
     terrain = []
     objects = []
     for p in cloud:
         pointsInRadius = kdtree.nnsInRadius(p, cloud.shape[1] - 1, radius)
-        # print(pointsInRadius.shape[0])
         if isObject(p, pointsInRadius, threshold):
             objects.append(p)
         else:
@@ -51,6 +50,7 @@ def KDTreeSearch(radius, threshold, cloud, kdtree):
     print('KDTree search took ', end - start, 'seconds')
     return terrain, objects
 
+
 if __name__ == '__main__':
     cloud = PointCloud()
     cloud.initializeData()
@@ -58,20 +58,20 @@ if __name__ == '__main__':
 
     cloud.pts = cloud.pts[argsort(cloud.pts[:, 0])]
 
-    g = GeoEqualCells()
-    g.initializeGeoEqualCells(cloud.pts,100,[1,1])
-    # kdtree = KDTree()
-    # kdtree.initializeKDTree(cloud.pts, cloud.pts.shape[1])
+    #g = GeoEqualCells()
+    #g.initializeGeoEqualCells(cloud.pts, 100, [1, 1])
+    kdtree = KDTree()
+    kdtree.initializeKDTree(cloud.pts, cloud.pts.shape[1] - 1)
     #
     # p1 = array([171962.554, 433217.960, 6.609])
     # res = kdtree.nnsInRadius(p1, p1.shape[0], 45.)
 
-    start = time.time()
+    # start = time.time()
     # terrain, objects = naiveSearch(5, 65, cloud.pts)
     terrain, objects = KDTreeSearch(5, 65, cloud.pts, kdtree)
-    terrain, objects = g.classifyPoints(5, 65)
-    end = time.time()
-    print(end)
+    # terrain, objects = g.classifyPoints(5, 65)
+    # end = time.time()
+    # print(end)
     cloud.drawFilteredPointCloud(objects, terrain)
 
     pnt = array([171930.554, 433217.960, 6.609])
