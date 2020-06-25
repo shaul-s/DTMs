4fea210659a53dbb4a8c3f8d9d9a55fa7cab6a96
Saul <shaul.s@campus.technion.ac.il>
2020-06-25 20:45:11 +0300

    interpolation

--- HW3/Utils.py
+++ HW3/Utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 from tkinter.filedialog import askopenfilenames
+
+
 # from __future__ import division
 
 def RightLeftCollinear(p1, p2, p3):
@@ -14,7 +16,6 @@ def RightLeftCollinear(p1, p2, p3):
         return 2
     if delta == 0:
         return 3
-
 
 
 def initializeData():
@@ -55,7 +56,7 @@ def loadHeightsGrid():
             with open(filename) as file:
                 lines = file.readlines()
 
-                for i,line in enumerate(lines):
+                for i, line in enumerate(lines):
                     line = line.split()
 
                     if len(line) < 3:
@@ -72,14 +73,15 @@ def loadHeightsGrid():
                         if line[0] == 'NODATA_value':
                             NODATA_value = float(line[1])
                     else:
-                        for j,height in enumerate(line):
-                            temp_points.append(np.array([xllcorner + j*cellsize, yllcorner + (nrows-(i+1-6))*cellsize, float(height)]))
+                        for j, height in enumerate(line):
+                            temp_points.append(np.array(
+                                [xllcorner + j * cellsize, yllcorner + (nrows - (i + 1 - 6)) * cellsize,
+                                 float(height)]))
 
         except:
             print('Oops! your file is not supported')
     points = np.asarray(temp_points)
-    return np.vstack(points),nrows,ncols,cellsize
-
+    return np.vstack(points), nrows, ncols, cellsize
 
 
 def ray_intersect_polygon(p, edge):
@@ -107,7 +109,7 @@ def ray_intersect_polygon(p, edge):
         return False
 
     # handling spacial cases
-    if p[1] == edge[0][1] or p[1] == edge[1][1]:  #[1] of point equal to[1] of an edge point
+    if p[1] == edge[0][1] or p[1] == edge[1][1]:  # [1] of point equal to[1] of an edge point
         p[1] += epsilon
         return ray_intersect_polygon(p, edge)
     if p[0] == edge[0][0] and p[0] == edge[1][0]:  # the point is in a vertical edge
@@ -127,12 +129,12 @@ def ray_intersect_polygon(p, edge):
 def line(p1, p2):
     A = (p1[1] - p2[1])
     B = (p2[0] - p1[0])
-    C = (p1[0]*p2[1] - p2[0]*p1[1])
+    C = (p1[0] * p2[1] - p2[0] * p1[1])
     return A, B, -C
 
 
 def intersection(L1, L2, z):
-    D  = L1[0] * L2[1] - L1[1] * L2[0]
+    D = L1[0] * L2[1] - L1[1] * L2[0]
     Dx = L1[2] * L2[1] - L1[1] * L2[2]
     Dy = L1[0] * L2[2] - L1[2] * L2[0]
     if D != 0:
@@ -142,6 +144,7 @@ def intersection(L1, L2, z):
     else:
         return 0
 
+
 def intersect(l1, l2, z):
     """
     finding point of intersection between two segments
@@ -150,8 +153,8 @@ def intersect(l1, l2, z):
     """
 
     epsilon = pow(10, -5)
-    a1 = (l1[1,1] - l1[0,1]) / (l1[1,0] - l1[0,0])
-    b1 = l1[0,1] - l1[0,0] * a1
+    a1 = (l1[1, 1] - l1[0, 1]) / (l1[1, 0] - l1[0, 0])
+    b1 = l1[0, 1] - l1[0, 0] * a1
     a2 = (l2[1, 1] - l2[0, 1]) / (l2[1, 0] - l2[0, 0])
     b2 = l2[0, 1] - l2[0, 0] * a2
     if a1 - a2 == 0:
@@ -159,10 +162,10 @@ def intersect(l1, l2, z):
     else:
         x_intersection = -(b1 - b2) / (a1 - a2)
         y_intersection = b1 + a1 * x_intersection
-        if ((l1[0,0] - x_intersection) * (x_intersection - l1[1,0]) >= 0 - epsilon and
-                (l2[0,0] - x_intersection) * (x_intersection - l2[1,0]) >= 0 - epsilon and
-                (l1[0,1] - y_intersection) * (y_intersection - l1[1,1]) >= 0 - epsilon and
-                (l2[0,1] - y_intersection) * (y_intersection - l2[1,1]) >= 0 - epsilon):
+        if ((l1[0, 0] - x_intersection) * (x_intersection - l1[1, 0]) >= 0 - epsilon and
+                (l2[0, 0] - x_intersection) * (x_intersection - l2[1, 0]) >= 0 - epsilon and
+                (l1[0, 1] - y_intersection) * (y_intersection - l1[1, 1]) >= 0 - epsilon and
+                (l2[0, 1] - y_intersection) * (y_intersection - l2[1, 1]) >= 0 - epsilon):
             return np.array([x_intersection, y_intersection, z, None])
         else:
             return 0
@@ -193,7 +196,7 @@ def isConvex(points):
     return True
 
 
-def rightTurn (p1, p2, p3):
+def rightTurn(p1, p2, p3):
     """
     defines if their is a right turn between 3 points
     Args:
@@ -204,10 +207,11 @@ def rightTurn (p1, p2, p3):
     Returns: (boolean) true if right turn, false otherwise
 
     """
-    if (p2[0] - p1[0])*(p3[1] - p1[1]) - (p3[0] - p1[0])*(p2[1]-p1[1]) < 0:
+    if (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1]) < 0:
         return True
     else:
         return False
+
 
 def anticlockwise(points):
     """
@@ -215,17 +219,17 @@ def anticlockwise(points):
 
     """
 
-    x_min = points[0,0]
+    x_min = points[0, 0]
     i_min = 0
     for i in range(len(points)):
-        if points[i,0] < x_min:
-            x_min = points[i,0]
+        if points[i, 0] < x_min:
+            x_min = points[i, 0]
             i_min = i
-    if i_min < len(points)-1:
+    if i_min < len(points) - 1:
         if i_min == 0:
             if rightTurn(points[i_min], points[i_min + 1], points[-1]):
                 points = list(reversed(points))
         elif rightTurn(points[i_min], points[i_min + 1], points[i_min - 1]):
             points = list(reversed(points))
     elif rightTurn(points[i_min], points[0], points[i_min - 1]):
-            points = list(reversed(points))
+        points = list(reversed(points))
--- HW5/hw5.py
+++ HW5/hw5.py
@@ -1,8 +1,10 @@
 from scipy import spatial as spat
 from scipy import ndimage
+from scipy import linalg as la
 from matplotlib import pyplot as plt
 from HW3.Delaunay import *
 from matplotlib.colors import hsv_to_rgb
+
 
 def hillShadeMap(map, azdeg, altdeg, cellSize):
     """
@@ -17,107 +19,207 @@ def hillShadeMap(map, azdeg, altdeg, cellSize):
     :type altdeg: float
     :rtype : np.array nXm,np.array nXm,np.array nXm
     """
-    azdeg = 360-(azdeg+90)
+    azdeg = 360 - (azdeg + 90)
     azdeg = np.radians(azdeg)
     altdeg = np.radians(altdeg)
 
     # calculate zanital degree
-    Zl = np.pi/2 - altdeg
-
+    Zl = np.pi / 2 - altdeg
 
     # calculate derivatives
-    Zx = ndimage.sobel(map, axis=0)/(cellSize)
-    Zy = ndimage.sobel(map, axis=1)/(cellSize)
+    Zx = ndimage.sobel(map, axis=0) / (cellSize)
+    Zy = ndimage.sobel(map, axis=1) / (cellSize)
 
     # calculate slope and aspect values
-    slope = np.arctan(Zx**2+Zy**2)
-    aspect = np.arctan2(Zy,Zx)
+    slope = np.arctan(Zx ** 2 + Zy ** 2)
+    aspect = np.arctan2(Zy, Zx)
 
     # if aspect is negative, add 360 deg
-    mask = (aspect < 0)*1
-    aspect = aspect + 2*np.pi*mask
+    mask = (aspect < 0) * 1
+    aspect = aspect + 2 * np.pi * mask
 
     # calculate shade value
-    shadeValue = 255*(np.cos(Zl)*np.cos(slope)+ np.sin(Zl)*np.sin(slope)*np.cos(azdeg-aspect))
+    shadeValue = 255 * (np.cos(Zl) * np.cos(slope) + np.sin(Zl) * np.sin(slope) * np.cos(azdeg - aspect))
     return shadeValue, slope, aspect
+
 
 def normalize(map):
     """normalize values to 0-1 range"""
-    return (map-np.min(map))/(np.max(map)-np.min(map))
+    return (map - np.min(map)) / (np.max(map) - np.min(map))
 
 
-def bcubic(DEM, nrows, ncol, cellSize, points):
-    """ bcubic interpolation on DEM """
-    # calculate derivatives
-    ZxTemp = (DEM.T[2:]-DEM.T[0:DEM.shape[1]-2])
-    Zx = ZxTemp.T/(2*cellSize)
-    Zx = Zx[1:Zx.shape[0]-1]
-    Zy = DEM[2:]-DEM[0:DEM.shape[0]-2]/(2*cellSize)
-    Zy = Zy[:,1:Zy.shape[1]-1]
-    Zxy = ZxTemp.T[2:]-ZxTemp.T[0:ZxTemp.T.shape[0]-2]/(4*cellSize*cellSize)
+# def bcubic(DEM, nrows, ncol, cellSize, points):
+#     """ bcubic interpolation on DEM """
+#     # calculate derivatives
+#     ZxTemp = (DEM.T[2:] - DEM.T[0:DEM.shape[1] - 2])
+#     Zx = ZxTemp.T / (2 * cellSize)
+#     Zx = Zx[1:Zx.shape[0] - 1]
+#     Zy = DEM[2:] - DEM[0:DEM.shape[0] - 2] / (2 * cellSize)
+#     Zy = Zy[:, 1:Zy.shape[1] - 1]
+#     Zxy = ZxTemp.T[2:] - ZxTemp.T[0:ZxTemp.T.shape[0] - 2] / (4 * cellSize * cellSize)
+#
+#     # find points's cells indices
+#
+#     Xinv = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [-3, -2, 3, -1], [2, 1, -2, 1]])
+#     XinvT = Xinv.T
 
-    # find points's cells indices
+def bcubic(oldDEM, point2d, idx, jdx):
+    def computeH(oldDEM, i, j):
+        h00 = oldDEM["DEM"][i, j]
+        h01 = (oldDEM["DEM"][i + 1, j] - oldDEM["DEM"][i - 1, j]) / (2 * oldDEM["cell_size"])
+        h10 = (oldDEM["DEM"][i, j + 1] - oldDEM["DEM"][i, j - 1]) / (2 * oldDEM["cell_size"])
+        h11 = (oldDEM["DEM"][i + 1, j + 1] - oldDEM["DEM"][i - 1, j + 1] - oldDEM["DEM"][i + 1, j - 1] +
+               oldDEM["DEM"][i - 1, j - 1]) / (4 * oldDEM["cell_size"] ** 2)
+        return np.array([[h00, h01], [h10, h11]])
+
+    # get x and y values of the cell upper right point in the old DEM
+    # x = oldDEM["cell_size"] * (point2d[0] - oldDEM["xll"]) % oldDEM["cell_size"]
+    # y = oldDEM["cell_size"] * (point2d[1] - oldDEM["yll"]) % oldDEM["cell_size"]
+    x = point2d[0]
+    y = point2d[1]
 
 
-    Xinv = np.array([[1,0,0,0],[0,1,0,0],[-3,-2,3,-1],[2,1,-2,1]])
-    XinvT = Xinv.T
+    # computing matrices for interpolation
+    H_matrices = []
+    for i in range(idx, idx + 2):
+        for j in range(jdx, jdx + 2):
+            H_matrices.append(computeH(oldDEM, i, j))
+
+    H = np.vstack((np.hstack((H_matrices[0], H_matrices[1])), np.hstack((H_matrices[2], H_matrices[-1]))))
+    X = Y = np.array(
+        [[1, 0, 0, 0], [0, 1, 0, 0], [1, oldDEM["cell_size"], oldDEM["cell_size"] ** 2, oldDEM["cell_size"] ** 3],
+         [0, 1, 2 * oldDEM["cell_size"], 3 * oldDEM["cell_size"] ** 2]])
+
+    # compute bi-cubic surface parameteres
+    A = np.dot(np.dot(la.inv(X), H), la.inv(Y.T))
+
+    # interpolate and return height
+    x_vec = np.array([1, x, x * x, x * x * x])
+    y_vec = np.array([1, y, y * y, y * y * y])
+    return np.dot(np.dot(x_vec, A), y_vec.T)
+
+
+def blinear(oldDEM, point2d, idx, jdx):
+    """
+
+    :param oldDEM:
+    :param point2d:
+    :param idx:
+    :param jdx:
+    :return:
+    """
+    # get x and y values of the cell upper right point in the old DEM
+    x = oldDEM["cell_size"] * (point2d[0] - oldDEM["xll"]) % oldDEM["cell_size"]
+    y = oldDEM["cell_size"] * (point2d[1] - oldDEM["yll"]) % oldDEM["cell_size"]
+
+    # compute bi-linear surface parameters
+    a = oldDEM["DEM"][jdx, idx]
+    b = (oldDEM["DEM"][jdx + 1, idx] - a) / oldDEM["cell_size"]
+    c = (oldDEM["DEM"][jdx, idx + 1] - a) / oldDEM["cell_size"]
+    d = (a - oldDEM["DEM"][jdx, idx + 1] - oldDEM["DEM"][jdx + 1, idx] + oldDEM["DEM"][jdx + 1, idx + 1]) / (
+            oldDEM["cell_size"] ** 2)
+
+    # compute interpolated height
+    return a + b * x + c * y + d * x * y
+
+
+def computeNewDEM(oldDEM, nrows, ncol, xll, yll, cellsize):
+    """
+    computing a new DEM based on a known DEM (oldDEM)
+    :param oldDEM: the known DEM has to be dict at shown
+    :param nrows: number of rows
+    :param ncol: number of columns
+    :param xll: lower left x
+    :param yll: lower left y
+    :param cellsize: size of cell
+    :return: new DEM
+    """
+    # check if user entered vars are legit
+    if (cellsize * ncol + xll > oldDEM["cell_size"] * oldDEM["ncol"] + oldDEM["xll"]) or \
+            (cellsize * nrows + yll > oldDEM["cell_size"] * oldDEM["nrows"] + oldDEM["yll"]):
+        print('the data you entered is invalid')
+        return
+
+    heightsgrid_xy = []
+    heightsgrid_z = []
+
+    for i in range(nrows + 1):
+        for j in range(ncol + 1):
+            #  create the XY grid
+            point2d = np.array([xll + j * cellsize, yll + i * cellsize])
+            heightsgrid_xy.append(point2d)
+            # find point's cell indexes in the old DEM so we can find them
+            idx = int((point2d[0] - oldDEM["xll"]) / oldDEM["cell_size"])
+            jdx = int((point2d[1] - oldDEM["yll"]) / oldDEM["cell_size"])
+            cell = np.array([idx, jdx])
+            # check if cell is on the edges, if it is use bi-linear, if it isn't use bi-cubic
+            if ((np.all((0, 0) < cell) and cell[1] < oldDEM["nrows"]) or \
+                    (np.all((0, 0) < cell) and cell[0] < oldDEM["ncol"]) or
+                    (np.all((0, oldDEM["nrows"]) < cell) and np.all(cell) < (oldDEM["ncol"], oldDEM["nrows"])) or
+                    (np.all((oldDEM["ncol"], 0) < cell) and np.all(cell) < (oldDEM["ncol"], oldDEM["nrows"]))):
+
+                heightsgrid_z.append(bcubic(oldDEM, point2d, idx, jdx))
+
+            else:
+                heightsgrid_z.append(blinear(oldDEM, point2d, idx, jdx))
+
+    heights_grid = np.hstack((np.vstack(heightsgrid_xy), np.vstack(heightsgrid_z)))
+    return heights_grid
 
 
 if __name__ == '__main__':
     # load data
-    points,nrows, ncol, cellSize = loadHeightsGrid()
-    HeightsGrid = np.reshape(points[:,2],(nrows,ncol))
+    points, nrows, ncol, cellSize = loadHeightsGrid()
+    HeightsGrid = np.reshape(points[:, 2], (nrows, ncol))
     # create triangulation
-    tri = spat.Delaunay(points[:, 0: 2])
+    # tri = spat.Delaunay(points[:, 0: 2])
+    #
+    # bcubic(HeightsGrid, nrows, ncol, cellSize, points)
+    #
+    # # fig, axs = plt.subplots(ncols=2,nrows=1)
+    # fig1, (heights, shades) = plt.subplots(1, 2)
+    #
+    # # draw triangulation
+    # # ax.triplot(points[:, 0], points[:, 1], tri.simplices)
+    #
+    # minHeight = np.min(points[:, 2])
+    # maxHeight = np.max(points[:, 2])
+    # grayLevel = (points[:, 2] - minHeight) / (maxHeight - minHeight) * 255
+    # points = np.hstack((points, np.reshape(grayLevel.T, (points.shape[0], 1))))
+    #
+    # # draw vertices in gray scale by height
+    # heights.scatter(points[:, 0], points[:, 1], c=points[:, 2], s=5, cmap='gray')
+    #
+    # # create hillshade map
+    # shadeValues, slope, aspect = hillShadeMap(HeightsGrid, 135, 45, cellSize)
+    # shades.imshow(shadeValues, cmap='gray')
+    # shades.set(xlabel='Illumination Intensity')
+    #
+    # # slopes and aspects maps
+    # fig2, (slopes, aspects, slopeAspect) = plt.subplots(1, 3)
+    #
+    # slopes.imshow(slope, cmap='gray')
+    # slopes.set(xlabel='slopes map')
+    #
+    # # aspect to HSV
+    # V = np.ones(aspect.shape)
+    # S = np.ones(aspect.shape)
+    # H = np.rad2deg(aspect) / 360
+    # aspectHSV = np.dstack((H, S, V))
+    # aspectRGB = hsv_to_rgb(aspectHSV)
+    # aspects.imshow(aspectRGB)
+    # aspects.set(xlabel='aspects map')
+    #
+    # # slope and aspect combine
+    # S = normalize(slope)
+    # slopeAspectHSV = np.dstack((H, S, V))
+    # slopeAspectRGB = hsv_to_rgb(slopeAspectHSV)
+    # slopeAspect.imshow(slopeAspectRGB)
+    # slopeAspect.set(xlabel='slopeAspects map')
+    # plt.show()
 
-    bcubic(HeightsGrid, nrows, ncol, cellSize, points)
+    oldDEM = {"DEM": HeightsGrid, "nrows": nrows, "ncol": ncol, "xll": 1000, "yll": 1000, "cell_size": cellSize}
 
-    # fig, axs = plt.subplots(ncols=2,nrows=1)
-    fig1, (heights, shades) = plt.subplots(1,2)
+    computeNewDEM(oldDEM, 20, 30, 1001, 1001, 10)
 
-    # draw triangulation
-    # ax.triplot(points[:, 0], points[:, 1], tri.simplices)
-
-    minHeight = np.min(points[:,2])
-    maxHeight = np.max(points[:,2])
-    grayLevel = (points[:,2] - minHeight)/(maxHeight-minHeight)*255
-    points = np.hstack((points,np.reshape(grayLevel.T,(points.shape[0],1))))
-
-    # draw vertices in gray scale by height
-    heights.scatter(points[:, 0], points[:, 1], c=points[:, 2], s=5, cmap='gray')
-
-    # create hillshade map
-    shadeValues, slope, aspect = hillShadeMap(HeightsGrid,135,45,cellSize)
-    shades.imshow(shadeValues,cmap='gray')
-    shades.set(xlabel='Illumination Intensity')
-
-    # slopes and aspects maps
-    fig2, (slopes, aspects, slopeAspect) = plt.subplots(1, 3)
-
-    slopes.imshow(slope,cmap='gray')
-    slopes.set(xlabel='slopes map')
-
-    # aspect to HSV
-    V = np.ones(aspect.shape)
-    S = np.ones(aspect.shape)
-    H = np.rad2deg(aspect)/360
-    aspectHSV = np.dstack((H, S, V))
-    aspectRGB = hsv_to_rgb(aspectHSV)
-    aspects.imshow(aspectRGB)
-    aspects.set(xlabel='aspects map')
-
-    # slope and aspect combine
-    S = normalize(slope)
-    slopeAspectHSV = np.dstack((H, S, V))
-    slopeAspectRGB = hsv_to_rgb(slopeAspectHSV)
-    slopeAspect.imshow(slopeAspectRGB)
-    slopeAspect.set(xlabel='slopeAspects map')
-    plt.show()
-
-
-
-    # for i, triangle in enumerate(tri.simplices):
-    #     tri_points = np.vstack((points[triangle[0]], points[triangle[1]], points[triangle[2]]))
-    #     centroid = np.average(tri_points, axis=0)
-    #     ax.annotate("({})".format(i), (centroid[0], centroid[1]))
-
+    print('hi')
